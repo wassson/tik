579c698dff2d251e794d727ccba0f64acdb34185
Austin Wasson <austinpwasson@gmail.com>
2023-12-11 16:21:56 -0500

    file permissions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::fs::File;
+use std::fs::{File, OpenOptions, Permissions};
+use std::os::unix::fs::PermissionsExt;
 use std::{process, fs};
 use std::{thread, time};
 use daemonize::Daemonize;
@@ -12,10 +13,10 @@ fn run() {
 }
 
 fn main() {
+    // TODO: Check if process is already running
     let home_dir = dirs::home_dir().unwrap();
     let new_dir = home_dir.join(".tik");
     let tik_dir = new_dir.as_path();
-
     let stdout = File::create(tik_dir.join("tik.out")).unwrap();
     let stderr = File::create(tik_dir.join("tik.err")).unwrap();
 
@@ -24,6 +25,17 @@ fn main() {
         _ => (),
     }
 
+    // Create .pid file and set permissions
+    let file = tik_dir.join("tik.pid");
+    let _f = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(&file);
+
+    let permissions = Permissions::from_mode(0o777);
+    let _ = std::fs::set_permissions(&file, permissions);
+
+    // TODO: Revisit to privileged_action()
     let daemonize = Daemonize::new()
         .pid_file(tik_dir.join("tik.pid")) 
         .chown_pid_file(true)
